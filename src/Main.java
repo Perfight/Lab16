5ca11075292e16c5a421b70000d065920761f614
Perfight <m0nter1327@mail.ru>
2023-03-06 08:14:31 +0300

    CombSort

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        System.out.print("Введите элементе массива: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        combSort(arr);
+        System.out.print("Отсортированный массив: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void combSort(int[] arr) {
+        int n = arr.length;
+        int step = n;
+        double factor = 1.247;
+        boolean sorted = false;
+
+        while (!sorted) {
+            step = (int) Math.floor(step / factor);
+            if (step <= 1) {
+                step = 1;
+                sorted = true;
+            }
+            int i = 0;
+            while (i + step < n) {
+                if (arr[i] > arr[i + step]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + step];
+                    arr[i + step] = temp;
+                    sorted = false;
+                }
+                i++;
+            }
+        }
+    }
+}
